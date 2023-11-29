f0bb331e984f4df2cb2b6c6c707206278f253a7e
Jessica <ladysinya@gmail.com>
2023-11-29 14:13:01 -0600

    Progress

--- holiday-tasks/script.js
+++ holiday-tasks/script.js
@@ -1,31 +1,87 @@
 class HTScript {
-    data;
-    // channel = new BroadcastChannel('ladysinya.github.io_holiday_tasks_broadcast_channel');
+    people;
+    lists;
+    gifts;
+
+    giftStatuses = [
+        "selected",
+        "purchased",
+        "shipped",
+        "received",
+        "wrapped",
+        "gifted",
+        "other"
+    ]
     
     async init() {
-        // this.channel.addEventListener('message', e => this.messageEventListener(e))
-        
-        await fetch('./data.json')
-        .then(async (response) => {
-            let responseContent = await response.text();
-            this.data = JSON.parse(responseContent);
+        await fetch('./Data/people.json')
+            .then(async (response) => {
+                let responseContent = await response.text();
+                this.people = JSON.parse(responseContent);
 
-            console.log(this.data);
-        });
+                console.log(this.people);
+            });
         
-        alert(JSON.stringify(this.data));
+        await fetch('./Data/gifts.json')
+            .then(async (response) => {
+                let responseContent = await response.text();
+                this.gifts = JSON.parse(responseContent);
+
+                console.log(this.gifts);
+            });
+
+        this.loadPeople();
+        this.setSectionListeners();
+        
     }
 
-    // messageEventListener(message) {
-    //     console.log('event', message);
+    loadPeople() {
+        const template = document.getElementById('people-card-template');
+        const elem = template.content.querySelector(".people-card");
 
-    //     if(message.data == 'next-question') {
-    //         this.loadNextQuestion();
-    //     }
+        const peopleSection = document.querySelector('.section-content [data-section="people"]');
 
-    //     if(message.data.includes('show-answer')){
-    //         const index = message.data.replace('show-answer-', '');
-    //         this.showAnswer(parseInt(index));
-    //     }
-    // }
+        this.people.forEach(p => {
+            const card = document.importNode(elem, true);
+            card.querySelector('.people-card-name').innerHTML = p.name;
+            card.querySelector('.people-card-budget').innerHTML = p.budget;
+
+            card.querySelector('.people-card-address-line1').innerHTML = p.address.line1
+            card.querySelector('.people-card-address-line2').innerHTML = p.address.line2
+            card.querySelector('.people-card-address-city').innerHTML = p.address.city
+            card.querySelector('.people-card-address-state').innerHTML = p.address.state
+            card.querySelector('.people-card-address-zip').innerHTML = p.address.zip
+
+            card.querySelector('.people-card-notes').innerHTML = p.notes;
+
+            peopleSection.append(card);
+        });
+    }
+
+    setSectionListeners() {
+        const sectionBtns = Array.from(document.querySelectorAll('.section-btn'));
+
+        sectionBtns.forEach(btn => {
+            btn.addEventListener('click', this.sectionBtnClicked.bind(this));
+        })
+    }
+
+    sectionBtnClicked(e) {
+        const sectionBtns = Array.from(document.querySelectorAll('.section-btn'));
+        
+        sectionBtns.forEach(btn => {
+            btn.classList.remove('section-active');
+            const sectionContent = document.querySelector(`.section-content section[data-section=${btn.dataset.section}]`);
+            sectionContent?.classList.add('display-none');
+        });
+
+        const targetBtn = e.target.matches('.section-btn')
+                    ? e.target
+                    : e.target.closest('.section-btn');
+
+        targetBtn.classList.add('section-active');
+
+        const newSectionContent = document.querySelector(`.section-content section[data-section=${targetBtn.dataset.section}]`);
+        newSectionContent?.classList.remove('display-none');
+    }
 }
