44003eb62b30a8b03614c714f92436680035d4c7
Jessica <ladysinya@gmail.com>
2024-07-12 21:44:08 -0500

    linking to local

---
+++ dnd-character-details/character.js
@@ -0,0 +1,11 @@
+class CharacterDetails {
+    async init() {
+        console.log('init');
+        var convert = require('xml-js');
+        // const xmlData = await readAsync('LVL2.xml', 'utf8');
+        
+        // const parseString = require('xml2js').parseString;
+        // const result = await parseString(xmlData);
+        // console.log(result);
+    }
+}
