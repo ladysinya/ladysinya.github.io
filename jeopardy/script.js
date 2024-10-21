fa8342ed44877abdfa24bfd83e97b9cd705518a2
Jessica <ladysinya@gmail.com>
2024-10-21 10:26:38 -0500

    things

--- jeopardy/script.js
+++ jeopardy/script.js
@@ -1,32 +1,33 @@
 class Jeopardy {
     data;
-    channel = new BroadcastChannel('ladysinya.github.io_jeopardy_broadcast_channel');
+    // channel = new BroadcastChannel('ladysinya.github.io_jeopardy_broadcast_channel');
+    theme = 'halloween'
 
 	async init() {
-        this.channel.addEventListener('message', e => this.messageEventListener(e));
+        // this.channel.addEventListener('message', e => this.messageEventListener(e));
 
         await fetch('./data.json')
             .then(async (response) => {
                 let responseContent = await response.text();
-                this.data = JSON.parse(responseContent);
+                this.data = JSON.parse(responseContent)[this.theme];
             });
 
         this.renderCards();
 	}
 
-    messageEventListener(message) {
-        console.log('event', message);
-    }
+    // messageEventListener(message) {
+    //     console.log('event', message);
+    // }
 
-    sendEvent(message) {
-        this.channel.postMessage(message);
-    }
+    // sendEvent(message) {
+    //     this.channel.postMessage(message);
+    // }
 
     renderCards() {
         const board = document.getElementById('board');
         board.innerHTML = '';
 
-        this.data.forEach((cat, i) => {
+        this.data.questions.forEach((cat, i) => {
             const catDiv = `
                 <div class="category-container" data-category-count="${i + 1}">
                     <div class="category-label">${cat.categoryName}</div>
@@ -37,56 +38,90 @@ class Jeopardy {
 
             const catContainer = board.querySelector(`.category-container[data-category-count="${i + 1}"]`);
             cat.items.forEach(item => {
-                const card = `
-                    <div 
-                        data-id="${item.id}" 
-                        data-question="${item.question}" 
-                        data-answer="${item.answer}" 
-                        data-daily-double="${item.value > 300 ? 'possible' : ''}"
-                        class="card">
-                            <div class="card-value">${item.value}</div>
-                            <div class="card-back">
-                                <div class="timer">15</div>
-                                <div class="answer-text">${item.answer}</div>
-                                <div class="question-text">${item.question}</div>
-                                <div class="team-bubbles">
-                                    <div data-team-color="red" class="team-bubble"></div>
-                                    <div data-team-color="orange" class="team-bubble"></div>
-                                    <div data-team-color="yellow" class="team-bubble"></div>
-                                    <div data-team-color="green" class="team-bubble"></div>
-                                    <div data-team-color="blue" class="team-bubble"></div>
-                                    <div data-team-color="purple" class="team-bubble"></div>
-                                    <div data-team-color="none" class="team-bubble"><i class="fa-duotone fa-solid fa-xmark"></i></div>
-                                </div>
-                            </div>
-                        </div>
-                `;
-
-                // <div class="question-icon"><i class="fa-duotone fa-solid fa-cauldron"></i></div>
-
-                const cardElem = Object.assign(document.createElement('div'), { innerHTML: card }).children[0];
-                catContainer.append(cardElem);
-                cardElem.querySelector('.timer').addEventListener('click', this.timerClicked.bind(this));
-                cardElem.querySelector('.timer').addEventListener('dblclick', this.timerDblClicked.bind(this));
-
-                const bubbles = Array.from(cardElem.querySelectorAll('.team-bubble'));
-                bubbles.forEach(bubble => {
-                    bubble.addEventListener('click', this.bubbleClicked.bind(this));
-                })
-            })
+                catContainer.append(this.buildCard(item));                
+            });
 
             const categoryLabel = catContainer.querySelector('.category-label');
             this.autoSizeFont(categoryLabel);
         });
+        
+        this.setDailyDouble();
+    }
 
-        const cards = Array.from(board.querySelectorAll('.card'));
-        cards.forEach(card => {
-            card.addEventListener('click', this.cardClicked.bind(this))
+    buildCard(item) {
+        const cardStr = `
+            <div 
+                data-id="${item.id}" 
+                data-question="${item.question}" 
+                data-answer="${item.answer}" 
+                data-value="${item.value}" 
+                data-daily-double="${item.value > 300 ? 'possible' : ''}"
+                class="card">
+                    <div class="card-value">${item.value}</div>
+                    <div class="card-back">
+                        <div class="timer">15</div>
+                        <div class="answer-text">${item.answer}</div>
+                        <div class="question-text">${item.question}</div>
+                        <div class="team-bubbles">
+                            <div data-team-color="red" class="team-bubble"></div>
+                            <div data-team-color="orange" class="team-bubble"></div>
+                            <div data-team-color="yellow" class="team-bubble"></div>
+                            <div data-team-color="green" class="team-bubble"></div>
+                            <div data-team-color="blue" class="team-bubble"></div>
+                            <div data-team-color="purple" class="team-bubble"></div>
+                            <div data-team-color="none" class="team-bubble"><i class="fa-duotone fa-solid fa-xmark"></i></div>
+                        </div>
+                    </div>
+                </div>
+        `;
+
+        const card = Object.assign(document.createElement('div'), { innerHTML: cardStr }).children[0];
+        card.querySelector('.timer').addEventListener('click', this.timerClicked.bind(this));
+        card.querySelector('.timer').addEventListener('dblclick', this.timerDblClicked.bind(this));
+        card.addEventListener('click', this.cardClicked.bind(this))
+
+        const bubbles = Array.from(card.querySelectorAll('.team-bubble'));
+        bubbles.forEach(bubble => {
+            bubble.addEventListener('click', this.bubbleClicked.bind(this));
         })
+        
+        return card;
+    }
 
-        const ddCards = cards.filter(c => c.dataset.dailyDouble == 'possible');
-        const dailyDouble = ddCards[Math.floor(Math.random() * ddCards.length)]
-        dailyDouble.dataset.dailyDouble = 'true';
+    setDailyDouble() {
+        const possibleDailyDoubleCards = Array.from(board.querySelectorAll('.card[data-daily-double="possible"]'));
+        const dailyDoubleCard = possibleDailyDoubleCards[Math.floor(Math.random() * possibleDailyDoubleCards.length)];
+
+        dailyDoubleCard.dataset.dailyDouble = 'true';
+        const dailyDoubleDiv = Object.assign(
+                                document.createElement('div'),
+                                { 
+                                    className: 'daily-double', 
+                                    innerHTML: `
+                                        <div class="daily-double-label">Daily Double!</div>
+                                        <label>What is your wager?</label>
+                                        <div class="input-group">
+                                            <input type="number" id="daily-double-wager-input">
+                                        </div>
+                                        <button id="daily-double-wager-button">Let's do this!</button>
+                                    `
+                                });
+        
+        dailyDoubleCard.insertBefore(dailyDoubleDiv, dailyDoubleCard.querySelector('.card-back'));
+        document.getElementById('daily-double-wager-button').addEventListener('click', this.dailyDoubleWagerClicked.bind(this));
+        document.getElementById('daily-double-wager-input').addEventListener('click', (e) => { e.stopPropagation(); });
+    }
+
+    dailyDoubleWagerClicked(e) {
+        e.stopPropagation();
+
+        const wager = document.getElementById('daily-double-wager-input').value;        
+        const card = e.currentTarget.closest('.card');
+
+        card.dataset.value = wager;
+        card.querySelector('.daily-double').remove();
+
+        this.resetTimer(card.querySelector('.timer'));
     }
 
     autoSizeFont(elem) {
@@ -99,23 +134,32 @@ class Jeopardy {
 
 	cardClicked(e) {
         const card = e.currentTarget;
-        if (card.matches('.card-open')){            
-            if (card.hasAttribute('data-scoring-team')) {
-                // const teamColor = card.getAttribute('data-scoring-team');
-                // const value = parseInt(card.querySelector('.card-value').innerHTML);
-                // const teamScoreElem = document.querySelector(`.team-score[data-team-color="${teamColor}"]`);
-                // if(teamScoreElem) {
-                //     let teamScore = parseInt(teamScoreElem.innerText);
-                //     teamScoreElem.innerText = teamScore + value;
-                // }
+        if (card.matches('.card-open')){
+            switch (card.dataset.scoringTeam) {
+                case 'none':
+                        if(card.dataset.dailyDouble == 'true') {
+                            card.dataset.scoringTeam = this.lastTeam;
+                            card.dataset.value = `-${card.dataset.value}`;
+                        }
 
-                this.updateScores();
-
-                card.setAttribute('data-disabled', 'true');
+                        card.setAttribute('data-disabled', 'true');
+                    break;
+                case null:                    
+                    break;
+                default:
+                    card.setAttribute('data-disabled', 'true');
+                break;
             }
-
+                
+            this.updateScores();
             card.classList.remove('card-open');
             card.previousElementSibling.remove();
+
+            const finalCheck = document.querySelector('.card:not([data-disabled="true"])');
+            if (finalCheck == null) {
+                this.timeForFinalJeopardy();
+            }
+
             return;
         }
 
@@ -124,7 +168,14 @@ class Jeopardy {
             openCard.classList.remove('card-open');
         });
 
-        const value = card.querySelector('.card-value').innerHTML;
+        if (card.dataset.dailyDouble == 'true') {
+            const otherBubbles = Array.from(card.querySelectorAll(`.team-bubble:not([data-team-color="${this.lastTeam}"],[data-team-color="none"])`));
+            otherBubbles.forEach(bubble => {
+                bubble.remove();
+            });
+        }
+
+        const value = card.querySelector('.card-value').innerText;
         const style = window.getComputedStyle(card);
 
         const disabledCard = `
@@ -141,6 +192,35 @@ class Jeopardy {
         }, 15000);
 	}
 
+    timeForFinalJeopardy() {
+        const card = `
+            <div 
+                data-id="${item.id}" 
+                data-question="${item.question}" 
+                data-answer="${item.answer}" 
+                data-value="${item.value}" 
+                data-daily-double="${item.value > 300 ? 'possible' : ''}"
+                class="card">
+                    <div class="card-value">${item.value}</div>
+                    <div class="card-back">
+                        <div class="timer">15</div>
+                        <div class="answer-text">${item.answer}</div>
+                        <div class="question-text">${item.question}</div>
+                        <div class="team-bubbles">
+                            <div data-team-color="red" class="team-bubble"></div>
+                            <div data-team-color="orange" class="team-bubble"></div>
+                            <div data-team-color="yellow" class="team-bubble"></div>
+                            <div data-team-color="green" class="team-bubble"></div>
+                            <div data-team-color="blue" class="team-bubble"></div>
+                            <div data-team-color="purple" class="team-bubble"></div>
+                            <div data-team-color="none" class="team-bubble"><i class="fa-duotone fa-solid fa-xmark"></i></div>
+                        </div>
+                    </div>
+                </div>
+        `;
+    }
+
+    lastTeam = 'red';
     bubbleClicked(e) {
         e.stopPropagation();
 
@@ -149,6 +229,7 @@ class Jeopardy {
 
         card.classList.add('show-question');
         card.setAttribute('data-scoring-team', teamColor);
+        this.lastTeam = teamColor == 'none' ? this.lastTeam : teamColor;
     }
 
     updateScores() {
@@ -157,9 +238,9 @@ class Jeopardy {
             const correctQuestions = Array.from(document.querySelectorAll(`.card[data-scoring-team="${elem.dataset.teamColor}"]`));
             let totalScore = 0;
             correctQuestions.forEach(q => {
-                totalScore += parseInt(q.querySelector('.card-value').innerText);
+                totalScore += parseInt(q.dataset.value);
             })
-            
+
             elem.innerText = totalScore;
         })
     }
