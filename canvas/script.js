a303fb223adb77788e4b84afdd44e2d3cb4225b7
Jessica <ladysinya@gmail.com>
2025-04-15 15:38:40 -0500

    canvas baseline

---
+++ canvas/script.js
@@ -0,0 +1,78 @@
+class CanvasDraw {
+	tagsToDraw = [		
+		// new TagDraw()
+	]
+	
+	async init() {
+		const img = new Image();
+		const documents = await getDocuments();
+		img.src = documents[0];
+		img.id = "my-img";
+		img.onload = (e) => {
+			document.body.appendChild(e.currentTarget);
+			drawBaseCanvas(e.currentTarget);
+		};
+
+		window.addEventListener("resize", () => {
+			drawBaseCanvas(document.getElementById("my-img"));
+		});
+
+		//documentDraw = new DocumentsDraw();
+
+		// this.tagsToDraw.forEach((sprite) =>  {
+		// 	sprite.draw(canvas)
+		// });
+	}
+
+	redrawCanvas() {
+		shitToDraw.forEach((sprite) =>  {
+			sprite.draw(canvas)
+		});
+	}
+}
+
+// class DocumentsDraw {
+// 	draw(canvas) {
+
+// 	}
+// }
+
+function drawBaseCanvas(img) {
+	console.log("doing canvas shit");
+	const canvas = document.getElementById("my-canvas");
+
+	const dpr = window.devicePixelRatio;
+	canvas.width = canvas.clientWidth * dpr;
+	canvas.height = canvas.clientHeight * dpr;
+
+	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = true;
+	ctx.imageSmoothingQuality = "high";
+
+	const hRatio = canvas.width / img.width;
+	const adjImgWidth = img.width * hRatio;
+	const adjImgHeight = img.height * hRatio;
+
+	const adjCanvasHeight = (img.height / img.width) * canvas.width;
+	canvas.dataset.adjCanvasHeight = `${adjCanvasHeight}px`;
+
+	const centerShift_x = (canvas.width - adjImgWidth) / 2;
+
+	// Setting these again seems hacky, but it works. I don't yet understand why.
+	canvas.width = canvas.clientWidth * dpr;
+	canvas.height = canvas.clientHeight * dpr;
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(img, centerShift_x, 0, adjImgWidth, adjImgHeight);
+}
+
+async function getDocuments() {
+	let docs;
+	await fetch(`./documents.json`)
+		.then(async (response) => {
+			let responseContent = await response.text();
+			docs = JSON.parse(responseContent);
+		});
+
+	return docs.documents;
+}
---
+++ canvas/tag.js
@@ -0,0 +1,13 @@
+class Tag {    
+    constructor(type, x, y, w, h) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    draw(canvas) {
+
+    }
+}
