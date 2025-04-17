59f3e2288ad88e63c8baaed8792910bd05f20c23
Jessica <ladysinya@gmail.com>
2025-04-17 09:13:27 -0500

    more

--- canvas/script.js
+++ canvas/script.js
@@ -10,13 +10,13 @@ class CanvasDraw {
 		img.id = "my-img";
 		img.onload = (e) => {
 			document.body.appendChild(e.currentTarget);
-			[this.cHeight, this.cWidth] = drawBaseCanvas(e.currentTarget);
-			this.redrawCanvas();
+			drawBaseCanvas(e.currentTarget);
+			// this.redrawCanvas();
 		};
 
 		window.addEventListener("resize", () => {
 			drawBaseCanvas(document.getElementById("my-img"));
-			this.redrawCanvas();
+			// this.redrawCanvas();
 		});
 
 		window.addEventListener('click', (e) => { console.log('offsetX', e.offsetX, 'offsetY', e.offsetY) })
@@ -38,34 +38,16 @@ class CanvasDraw {
 // }
 
 function drawBaseCanvas(img) {
-	console.log("doing canvas shit");
 	const canvas = document.getElementById("my-canvas");
-
-	const dpr = window.devicePixelRatio;
-	canvas.width = canvas.clientWidth * dpr;
-	canvas.height = canvas.clientHeight * dpr;
-
 	const ctx = canvas.getContext("2d");
-	ctx.imageSmoothingEnabled = true;
-	ctx.imageSmoothingQuality = "high";
 
-	const hRatio = canvas.width / img.width;
-	const adjImgWidth = img.width * hRatio;
-	const adjImgHeight = img.height * hRatio;
+	// now we draw the image
+	const imgRatio = (img.height / img.width);
 
-	const adjCanvasHeight = (img.height / img.width) * canvas.width;
-	canvas.dataset.adjCanvasHeight = `${adjCanvasHeight}px`;
-
-	const centerShift_x = (canvas.width - adjImgWidth) / 2;
-
-	// Setting these again seems hacky, but it works. I don't yet understand why.
-	canvas.width = canvas.clientWidth * dpr;
-	canvas.height = canvas.clientHeight * dpr;
+	canvas.dataset.adjCanvasHeight = `${imgRatio * canvas.clientWidth}px`;
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(img, centerShift_x, 0, adjImgWidth, adjImgHeight);
-
-	return [canvas.height, canvas.width];
+	ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 }
 
 async function getDocuments() {
